27260bca219d4dac5c6fe580d23233f085df6f6a
Stefan Haan <stefan@localhost.localdomain>
2014-08-05 22:34:24 +0200

    Revert "Revert "Added Int Parser""
    This reverts commit 587869c220015d62fd9e48aa587f28aa715b4e77.

---
+++ src/production/DAEIntArray.java
@@ -0,0 +1,18 @@
+package production;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+/**
+ * Created by Stefan Haan on 8/5/14.
+ */
+public class DAEIntArray implements DAEElement {
+    IntBuffer dataBuffer;
+    DAEIntArray (int[] data){
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4 * data.length);
+        dataBuffer = byteBuffer.asIntBuffer();
+    }
+    public int getCount() {
+        return dataBuffer.capacity();
+    }
+}
